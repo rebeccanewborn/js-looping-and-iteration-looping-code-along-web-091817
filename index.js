cfc5de3a0c546528a1a8731308a67dd90f9afeaf
rebeccanewborn <rjn58@cornell.edu>
2017-10-30 09:41:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(employees){
+  for (let i = 0; i < employees.length; i++){
+    console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+  }
+  return employees;
+}
+
+function tailsNeverFails(){
+  function maybeTails () {
+    return Math.random() >= 0.5;
+  }
+  let counter = 0
+  while (maybeTails()){
+    counter ++
+  }
+  return `You got ${counter} tails in a row!`;
+}
